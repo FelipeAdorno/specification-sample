c2a7d9142396e54f4c0d9c04477107c56402c725
Felipe Adorno <felipeadsc@gmail.com>
2016-12-19 19:07:12 -0200

    Update PersonRestService.java

--- src/main/java/br/com/specification/sample/rest/PersonRestService.java
+++ src/main/java/br/com/specification/sample/rest/PersonRestService.java
@@ -5,6 +5,7 @@ import br.com.specification.sample.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +22,7 @@ public class PersonRestService {
     @Autowired
     private PersonService personService;
 
+    @GetMapping
     public Page<Person> list(@RequestParam(required = false) String name,
                              @RequestParam(required = false) Integer cpf,
                              @RequestParam(required = false) Integer phone,
